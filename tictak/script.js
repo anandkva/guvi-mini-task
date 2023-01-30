526dbac69174f6332d6a2763a2b02f86e136d5a2
Anand Kumar <anand.kva00@gmail.com>
2023-01-30 10:47:17 +0530

    changes

--- tictak/script.js
+++ tictak/script.js
@@ -1,99 +1,109 @@
 const Game = () => {
-    var game = document.getElementById('game');
-    var boxes = document.querySelectorAll('li');
-    var resetGame = document.getElementById('reset-game');
-    var turnDisplay = document.getElementById('whos-turn');
-    var gameMessages = document.getElementById('game-messages');
-    var playerOneScoreCard = document.getElementById('player-one-score');
-    var playerTwoScoreCard = document.getElementById('player-two-score');
-    var context = { 'player1': 'x', 'player2': 'o' };
-    var board = [];
-    var playerOneScore = 0;
-    var playerTwoScore = 0;
-    var turns;
-    var currentContext;
-    var init = () => {
-        turns = 0;
-        currentContext = computeContext();
-        board[0] = new Array(3);
-        board[1] = new Array(3);
-        board[2] = new Array(3);
-        for (var i = 0; i < boxes.length; i++) {
-            boxes[i].addEventListener('click', clickHandler, false);
+  const game = document.getElementById("game");
+  const boxes = document.querySelectorAll("li");
+  const resetGame = document.getElementById("reset-game");
+  const turnDisplay = document.getElementById("whos-turn");
+  const gameMessages = document.getElementById("game-messages");
+  const playerOneScoreCard = document.getElementById("player-one-score");
+  const playerTwoScoreCard = document.getElementById("player-two-score");
+  const context = { player1: "x", player2: "o" };
+  const board = [];
+  const playerOneScore = 0;
+  const playerTwoScore = 0;
+  let turns;
+  let currentContext;
+  const init = () => {
+    turns = 0;
+    currentContext = computeContext();
+    board[0] = new Array(3);
+    board[1] = new Array(3);
+    board[2] = new Array(3);
+    for (let i = 0; i < boxes.length; i++) {
+      boxes[i].addEventListener("click", clickHandler, false);
+    }
+    resetGame.addEventListener("click", resetGameHandler, false);
+  };
+  const computeContext = () => {
+    return turns % 2 == 0 ? context.player1 : context.player2;
+  };
+  const clickHandler = function() {
+    this.removeEventListener("click", clickHandler);
+    this.className = currentContext;
+    this.innerHTML = currentContext;
+    const pos = this.getAttribute("data-pos").split(",");
+    board[pos[0]][pos[1]] = computeContext() == "x" ? 1 : 0;
+    if (checkStatus()) {
+      gameWon();
+    }
+    turns++;
+    currentContext = computeContext();
+    turnDisplay.className = currentContext;
+  };
+  const checkStatus = () => {
+    let used_boxes = 0;
+    for (let rows = 0; rows < board.length; rows++) {
+      let row_total = 0;
+      let column_total = 0;
+      for (let columns = 0; columns < board[rows].length; columns++) {
+        row_total += board[rows][columns];
+        column_total += board[columns][rows];
+        if (typeof board[rows][columns] !== "undefined") {
+          used_boxes++;
         }
-        resetGame.addEventListener('click', resetGameHandler, false);
+      }
+      const diagonal_tl_br = board[0][0] + board[1][1] + board[2][2];
+      const diagonal_tr_bl = board[0][2] + board[1][1] + board[2][0];
+      if (
+        diagonal_tl_br == 0 ||
+        diagonal_tr_bl == 0 ||
+        diagonal_tl_br == 3 ||
+        diagonal_tr_bl == 3
+      ) {
+        return true;
+      }
+      if (
+        row_total == 0 ||
+        column_total == 0 ||
+        row_total == 3 ||
+        column_total == 3
+      ) {
+        return true;
+      }
+      if (used_boxes == 9) {
+        gameDraw();
+      }
     }
-    var computeContext = () => {
-        return (turns % 2 == 0) ? context.player1 : context.player2;
+  };
+  const gameWon = () => {
+    clearEvents();
+    gameMessages.className = "player-" + computeContext() + "-win";
+    switch (computeContext()) {
+      case "x":
+        playerOneScoreCard.innerHTML = ++playerOneScore;
+        break;
+      case "o":
+        playerTwoScoreCard.innerHTML = ++playerTwoScore;
     }
-    var clickHandler = function () {
-        this.removeEventListener('click', clickHandler);
-        this.className = currentContext;
-        this.innerHTML = currentContext;
-        var pos = this.getAttribute('data-pos').split(',');
-        board[pos[0]][pos[1]] = computeContext() == 'x' ? 1 : 0;
-        if (checkStatus()) {
-            gameWon();
-        }
-        turns++;
-        currentContext = computeContext();
-        turnDisplay.className = currentContext;
+  };
+  const gameDraw = () => {
+    gameMessages.className = "draw";
+    clearEvents();
+  };
+  const clearEvents = () => {
+    for (let i = 0; i < boxes.length; i++) {
+      boxes[i].removeEventListener("click", clickHandler);
     }
-    var checkStatus = () => {
-        var used_boxes = 0;
-        for (var rows = 0; rows < board.length; rows++) {
-            var row_total = 0;
-            var column_total = 0;
-            for (var columns = 0; columns < board[rows].length; columns++) {
-                row_total += board[rows][columns];
-                column_total += board[columns][rows];
-                if (typeof board[rows][columns] !== "undefined") {
-                    used_boxes++;
-                }
-            }
-            var diagonal_tl_br = board[0][0] + board[1][1] + board[2][2];
-            var diagonal_tr_bl = board[0][2] + board[1][1] + board[2][0];
-            if (diagonal_tl_br == 0 || diagonal_tr_bl == 0 || diagonal_tl_br == 3 || diagonal_tr_bl == 3) {
-                return true;
-            }
-            if (row_total == 0 || column_total == 0 || row_total == 3 || column_total == 3) {
-                return true;
-            }
-            if (used_boxes == 9) {
-                gameDraw();
-            }
-        }
+  };
+  const resetGameHandler = () => {
+    clearEvents();
+    init();
+    for (let i = 0; i < boxes.length; i++) {
+      boxes[i].className = "";
+      boxes[i].innerHTML = "";
     }
-    var gameWon = function () {
-        clearEvents();
-        gameMessages.className = 'player-' + computeContext() + '-win';
-        switch (computeContext()) {
-            case 'x':
-                playerOneScoreCard.innerHTML = ++playerOneScore;
-                break;
-            case 'o':
-                playerTwoScoreCard.innerHTML = ++playerTwoScore;
-        }
-    }
-    var gameDraw = function () {
-        gameMessages.className = 'draw';
-        clearEvents();
-    }
-    var clearEvents = function () {
-        for (var i = 0; i < boxes.length; i++) {
-            boxes[i].removeEventListener('click', clickHandler);
-        }
-    }
-    var resetGameHandler = function () {
-        clearEvents();
-        init();
-        for (var i = 0; i < boxes.length; i++) {
-            boxes[i].className = '';
-            boxes[i].innerHTML = '';
-        }
-        turnDisplay.className = currentContext;
-        gameMessages.className = '';
-    }
-    game && init();
-}
+    turnDisplay.className = currentContext;
+    gameMessages.className = "";
+  };
+  game && init();
+};
 Game();
